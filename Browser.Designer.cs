7eaba339eb97ebe0386d95fb3a2005110148d55f
Datorium <hello@datorium.lv>
2020-03-27 17:06:49 +0200

    version 17:06

--- Browser.Designer.cs
+++ Browser.Designer.cs
@@ -30,6 +30,11 @@
         {
             this.buttonGo = new System.Windows.Forms.Button();
             this.AddressBar = new System.Windows.Forms.TextBox();
+            this.Tabs = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.Tabs.SuspendLayout();
             this.SuspendLayout();
             // 
             // buttonGo
@@ -50,15 +55,59 @@
             this.AddressBar.Size = new System.Drawing.Size(381, 38);
             this.AddressBar.TabIndex = 1;
             // 
-            // Form1
+            // Tabs
+            // 
+            this.Tabs.Controls.Add(this.tabPage1);
+            this.Tabs.Controls.Add(this.tabPage2);
+            this.Tabs.Location = new System.Drawing.Point(12, 82);
+            this.Tabs.Name = "Tabs";
+            this.Tabs.SelectedIndex = 0;
+            this.Tabs.Size = new System.Drawing.Size(749, 329);
+            this.Tabs.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(741, 300);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(741, 300);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(465, 12);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(60, 38);
+            this.buttonAdd.TabIndex = 3;
+            this.buttonAdd.Text = "+";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // Browser
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.Tabs);
             this.Controls.Add(this.AddressBar);
             this.Controls.Add(this.buttonGo);
-            this.Name = "Form1";
+            this.Name = "Browser";
             this.Text = "Form1";
+            this.Resize += new System.EventHandler(this.Browser_Resize);
+            this.Tabs.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -68,6 +117,10 @@
 
         private System.Windows.Forms.Button buttonGo;
         private System.Windows.Forms.TextBox AddressBar;
+        private System.Windows.Forms.TabControl Tabs;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button buttonAdd;
     }
 }
 
--- Browser.cs
+++ Browser.cs
@@ -20,19 +20,37 @@ namespace ChromiumBrowser
         {
             InitializeComponent();
             InitializeBrowser();
+            InitializeTabs();
         }
         private void InitializeBrowser()
         {
             Cef.Initialize(new CefSettings());
-            browser = new ChromiumWebBrowser("www.google.com");
-            this.Controls.Add(browser);
-            browser.Dock = DockStyle.Bottom;
-            browser.Height = ClientRectangle.Height - 50;
         }
 
         private void buttonGo_Click(object sender, EventArgs e)
         {
             browser.Load(AddressBar.Text);
+        }
+
+        private void InitializeTabs()
+        {
+            Tabs.Dock = DockStyle.Bottom;
+            Tabs.Height = ClientRectangle.Height - 50;
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            TabPage newTab = new TabPage();
+            browser = new ChromiumWebBrowser("www.google.com");
+            newTab.Text = "tab" + (Tabs.TabCount + 1).ToString();
+            newTab.Controls.Add(browser);
+            Tabs.Controls.Add(newTab);
+            browser.Dock = DockStyle.Fill;
+        }
+
+        private void Browser_Resize(object sender, EventArgs e)
+        {
+            Tabs.Height = ClientRectangle.Height - 50;
         }
     }
 }
