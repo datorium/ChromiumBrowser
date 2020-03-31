919bc5d2ccc5d39859d153b4bcc83bc502fe83ed
Datorium <hello@datorium.lv>
2020-03-31 16:13:31 +0300

    version 16:14

--- Browser.Designer.cs
+++ Browser.Designer.cs
@@ -28,42 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.buttonGo = new System.Windows.Forms.Button();
-            this.AddressBar = new System.Windows.Forms.TextBox();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Browser));
             this.Tabs = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.buttonAddTab = new System.Windows.Forms.Button();
-            this.buttonRefresh = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.GoButton = new System.Windows.Forms.ToolStripButton();
+            this.AddressBar = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
             this.Tabs.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // buttonGo
-            // 
-            this.buttonGo.Location = new System.Drawing.Point(12, 12);
-            this.buttonGo.Name = "buttonGo";
-            this.buttonGo.Size = new System.Drawing.Size(49, 41);
-            this.buttonGo.TabIndex = 0;
-            this.buttonGo.Text = "Go";
-            this.buttonGo.UseVisualStyleBackColor = true;
-            this.buttonGo.Click += new System.EventHandler(this.buttonGo_Click);
-            // 
-            // AddressBar
-            // 
-            this.AddressBar.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.AddressBar.Location = new System.Drawing.Point(67, 12);
-            this.AddressBar.Name = "AddressBar";
-            this.AddressBar.Size = new System.Drawing.Size(347, 38);
-            this.AddressBar.TabIndex = 1;
             // 
             // Tabs
             // 
             this.Tabs.Controls.Add(this.tabPage1);
             this.Tabs.Controls.Add(this.tabPage2);
-            this.Tabs.Location = new System.Drawing.Point(12, 74);
+            this.Tabs.Location = new System.Drawing.Point(12, 43);
             this.Tabs.Name = "Tabs";
             this.Tabs.SelectedIndex = 0;
-            this.Tabs.Size = new System.Drawing.Size(807, 396);
+            this.Tabs.Size = new System.Drawing.Size(807, 427);
             this.Tabs.TabIndex = 2;
             // 
             // tabPage1
@@ -71,7 +55,7 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 25);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(799, 367);
+            this.tabPage1.Size = new System.Drawing.Size(799, 398);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "tabPage1";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -86,55 +70,67 @@
             this.tabPage2.Text = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
-            // buttonAddTab
+            // toolStrip1
             // 
-            this.buttonAddTab.Location = new System.Drawing.Point(420, 12);
-            this.buttonAddTab.Name = "buttonAddTab";
-            this.buttonAddTab.Size = new System.Drawing.Size(49, 41);
-            this.buttonAddTab.TabIndex = 3;
-            this.buttonAddTab.Text = "+";
-            this.buttonAddTab.UseVisualStyleBackColor = true;
-            this.buttonAddTab.Click += new System.EventHandler(this.buttonAddTab_Click);
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.GoButton,
+            this.AddressBar,
+            this.toolStripButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(831, 27);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
             // 
-            // buttonRefresh
+            // GoButton
             // 
-            this.buttonRefresh.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonRefresh.Location = new System.Drawing.Point(475, 12);
-            this.buttonRefresh.Name = "buttonRefresh";
-            this.buttonRefresh.Size = new System.Drawing.Size(49, 41);
-            this.buttonRefresh.TabIndex = 4;
-            this.buttonRefresh.Text = "🗘";
-            this.buttonRefresh.UseVisualStyleBackColor = true;
-            this.buttonRefresh.Click += new System.EventHandler(this.buttonRefresh_Click);
+            this.GoButton.Image = ((System.Drawing.Image)(resources.GetObject("GoButton.Image")));
+            this.GoButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.GoButton.Name = "GoButton";
+            this.GoButton.Size = new System.Drawing.Size(52, 24);
+            this.GoButton.Text = "Go";
+            // 
+            // AddressBar
+            // 
+            this.AddressBar.Name = "AddressBar";
+            this.AddressBar.Size = new System.Drawing.Size(400, 27);
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(29, 24);
+            this.toolStripButton1.Text = "toolStripButton1";
             // 
             // Browser
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(831, 482);
-            this.Controls.Add(this.buttonRefresh);
-            this.Controls.Add(this.buttonAddTab);
+            this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.Tabs);
-            this.Controls.Add(this.AddressBar);
-            this.Controls.Add(this.buttonGo);
             this.Name = "Browser";
             this.Text = "Form1";
             this.Resize += new System.EventHandler(this.Browser_Resize);
             this.Tabs.ResumeLayout(false);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button buttonGo;
-        private System.Windows.Forms.TextBox AddressBar;
         private System.Windows.Forms.TabControl Tabs;
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
-        private System.Windows.Forms.Button buttonAddTab;
-        private System.Windows.Forms.Button buttonRefresh;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton GoButton;
+        private System.Windows.Forms.ToolStripTextBox AddressBar;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
     }
 }
 
